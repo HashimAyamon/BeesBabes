f358e8c16e1952c55a926cd9a360348db54af911
Hashim. A <162900023+HashimAyamon@users.noreply.github.com>
2025-03-01 14:12:43 +0530

    server setup almost done

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import productRoutes from './routes/productRoutes.js'
 
+
+const PORT = process.env.PORT || 5000
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.use(express.json());
+app.use(helmet());     //for security
+app.use(cors());     //for security
+app.use(morgan("dev"));   //to get consol log the requests
+
+
+app.use("/api/products" ,productRoutes);
+
+
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
 });
